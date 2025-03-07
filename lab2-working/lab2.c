3f9ce2437c7387064d1b87c66e2056bf55579099
FITD-fash <ztm2106@columbia.edu>
2025-03-06 20:27:30 -0500

    Update lab2.c

--- lab2-working/lab2.c
+++ lab2-working/lab2.c
@@ -60,10 +60,10 @@ char keycode_to_ascii(uint8_t keycode, uint8_t modifiers)
         return '\b'; // Backspace
     case 0x28:
         return '\n'; // Enter
-    // case 0x2d:
-    //     return '-';
-    // case 0x2e:
-    //     return '=';
+    case 0x2d:
+        return '-';
+    case 0x2e:
+        return '=';
     case 0x34:
         return '"';
     case 0x36:
